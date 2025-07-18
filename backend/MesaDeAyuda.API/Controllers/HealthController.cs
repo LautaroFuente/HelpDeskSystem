baa72cfc5d03d08faf82d04d7aaaae4f2e315600
LautaroFuente <lautaro.fuente@yahoo.com>
2025-07-18 12:52:46 -0300

    T1.4: Create API base with HealtController and configurate Swagger

---
+++ backend/MesaDeAyuda.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MesaDeAyuda.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API funcionando correctamente.");
+        }
+    }
+}
