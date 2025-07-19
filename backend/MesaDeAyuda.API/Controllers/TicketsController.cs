58ff0fcefdd170cbc7a1ece5896eda2b30b89d9a
LautaroFuente <lautaro.fuente@yahoo.com>
2025-07-19 16:05:52 -0300

    add endpoint /ticket POST

---
+++ backend/MesaDeAyuda.API/Controllers/TicketsController.cs
@@ -0,0 +1,17 @@
+﻿using MesaDeAyuda.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MesaDeAyuda.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateTicket([FromBody] Ticket ticket)
+        {
+            // For now just return the ticket as confirmation
+            return Created("", ticket);
+        }
+    }
+}
