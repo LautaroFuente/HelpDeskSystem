3db454ac4a75a013f95fe36d4a0a63994144f4e6
LautaroFuente <lautaro.fuente@yahoo.com>
2025-07-24 19:20:16 -0300

    create more unit test for get ticket of ticket service in backend with Xunit

--- backend/MesaDeAyuda/MesaDeAyuda.Test/TicketServiceTest.cs
+++ backend/MesaDeAyuda/MesaDeAyuda.Test/TicketServiceTest.cs
@@ -19,26 +19,66 @@ namespace MesaDeAyuda.API.MesaDeAyuda.Test
         }
 
         [Fact]
-        public async Task CrearTicket_DeberiaGuardarTicketEnBase()
+        public async Task CreateTicket_ShouldBeSaveInDB()
         {
             // Arrange (configuración del test)
             var context = GetInMemoryDbContext();
             var service = new TicketService(context);
 
-            var nuevoTicket = new CreateTicketDTO
+            var newTicket = new CreateTicketDTO
             {
                 Title = "Test de prueba",
                 Description = "Probando xUnit",
             };
 
             // Act (acción que queremos testear)
-            var ticket = await service.CreateTicket(nuevoTicket);
+            var ticket = await service.CreateTicket(newTicket);
 
             // Assert (verificación de lo esperado)
-            var ticketEnBase = await context.Tickets.FindAsync(ticket.Id);
+            var ticketInBD = await context.Tickets.FindAsync(ticket.Id);
 
-            Assert.NotNull(ticketEnBase);
-            Assert.Equal("Test de prueba", ticketEnBase.Title);
+            Assert.NotNull(ticketInBD);
+            Assert.Equal("Test de prueba", ticketInBD.Title);
         }
+
+        [Fact]
+        public async Task GetTicketById_ShouldGetTicketIfExist()
+        {
+            // Arrange
+            // Arrange (configuración del test)
+            var context = GetInMemoryDbContext();
+            var service = new TicketService(context);
+
+            var newTicket = new CreateTicketDTO
+            {
+                Title = "Ticket prueba",
+                Description = "Probando xUnit",
+            };
+            var ticketCreated = await service.CreateTicket(newTicket);
+
+            // Act
+            var ticketGet = await service.GetTicketById(ticketCreated.Id);
+
+            // Assert
+            Assert.NotNull(ticketGet);
+            Assert.Equal(ticketCreated.Id, ticketGet.Id);
+            Assert.Equal("Ticket prueba", ticketGet.Title);
+        }
+
+        [Fact]
+        public async Task GetTicketById_ShouldBeNullIfNotExist()
+        {
+            // Arrange
+            var context = GetInMemoryDbContext();
+            var service = new TicketService(context);
+
+            // Act
+            var result = await service.GetTicketById(99999);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+
     }
 }
